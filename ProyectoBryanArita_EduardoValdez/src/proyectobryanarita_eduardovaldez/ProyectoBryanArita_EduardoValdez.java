2bec8ab5ca0a95f4a4890c28873ce06db87343ba
aritabryan <147879047+aritabryan@users.noreply.github.com>
2024-03-19 17:21:20 -0600

    Add files via upload

---
+++ ProyectoBryanArita_EduardoValdez/src/proyectobryanarita_eduardovaldez/ProyectoBryanArita_EduardoValdez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectobryanarita_eduardovaldez;
+
+/**
+ *
+ * @author BJAL
+ */
+public class ProyectoBryanArita_EduardoValdez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
